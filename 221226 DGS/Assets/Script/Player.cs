76128081b417094fc68af837b992a704ca57d028
yejin9337 <103083746+yejin9337@users.noreply.github.com>
2022-12-30 00:27:53 +0900

    [Feat]Player1 스킬 완료
    1. 스킬 구현
    2. 스킬 쿨타임 구현

--- 221226 DGS/Assets/Script/Player.cs
+++ 221226 DGS/Assets/Script/Player.cs
@@ -11,6 +11,11 @@ public class Player : MonoBehaviour
     [SerializeField] private int _defense = 5;
     [SerializeField] private Player _enemy;
 
+    private int _damage { get; set; }
+    private const int _firstSkillCoolTime = 3;
+    private int _currentFirstSkillCoolTime = 0;
+    private const int _secondSkillCoolTime = 3;
+    private int _currentSecondSkillCoolTime = 0;
 
     private float _actGauge = 0;
     private int _currentHP;
@@ -43,17 +48,54 @@ public class Player : MonoBehaviour
     {
         Debug.Log($"{this.gameObject.name} 의 공격!");
 
+        _damage = 0;
+        _currentFirstSkillCoolTime--;
+        _currentSecondSkillCoolTime--;
+
+        if (_currentSecondSkillCoolTime < 0)
+        {
+            _damage = SecondSkill();
+        }
+        else if (_currentFirstSkillCoolTime < 0)
+        {
+            _damage = FirstSkill();
+        }
+        else
+        {
+            _damage = _power;
+        }
+
         // 상대방의 OnDamaged 호출하기
-        _enemy.OnDamaged(_power);
+        _enemy.OnDamaged(_damage);
 
         // 나의 행동 게이지 0으로 만들기
         _actGauge = 0;
     }
 
+    private int FirstSkill()
+    {
+        _currentHP += (int)(_maxHP * 0.2f);
+        _hpSlider.value = _currentHP * 0.01f;
+        _currentFirstSkillCoolTime = _firstSkillCoolTime;
+        return _damage;
+    }
+
+    private int SecondSkill()
+    {
+        _damage = 50 + (int)(_defense * 0.1f);
+        _currentSecondSkillCoolTime = _secondSkillCoolTime;
+        return _damage;
+    }
+
     public void OnDamaged(int damege)
     {
-        _currentHP = _currentHP - (damege - _defense)
-            ;
+        if(damege <= _defense)
+        {
+            return;
+        }
+
+        _currentHP = _currentHP - (damege - _defense);
+            
         _hpSlider.value = _currentHP * 0.01f;
         _damageUI.StartEffect((damege - _defense));
 
